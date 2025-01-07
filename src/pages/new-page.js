42144af1c3a8642c4265e664c0f9f7263750a98b
Zevik <zaviner@gmail.com>
2025-01-07 14:31:55 +0200

    Initial commit

---
+++ src/pages/new-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const NewPage = () => (
+  <div>
+    <h1>New Page</h1>
+    <p>Welcome to your new Gatsby page.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default NewPage;
